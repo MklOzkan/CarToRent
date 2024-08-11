6cee445940162be6a99ac4d906242a9eeb6b29a8
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-08-11 10:59:00 +0200

    Category enum class

--- src/main/java/org/project/cartorent/entity/enums/Category.java
+++ src/main/java/org/project/cartorent/entity/enums/Category.java
@@ -1,4 +1,33 @@
 package org.project.cartorent.entity.enums;
 
 public enum Category {
+
+    ECONOMY,
+    COMPACT,
+    INTERMEDIATE,
+    STANDARD,
+    FULL_SIZE,
+    PREMIUM,
+    LUXURY,
+    CONVERTIBLE,
+    SUV,
+    VAN,
+    TRUCK,
+    HYBRID,
+    ELECTRIC,
+    SPORTS_CAR,
+    EXOTIC,
+    OFF_ROAD,
+    PICKUP_TRUCK,
+    MINIVAN,
+    STATION_WAGON,
+    COUPE,
+    HATCHBACK,
+    ROADSTER,
+    LIMOUSINE,
+    CABRIOLET,
+    MUSCLE_CAR,
+    SUP
+
+
 }
